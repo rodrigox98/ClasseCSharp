7cf65cdbb3a2b6fe112577e7ab149ca18e413fdb
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-07-19 21:11:04 -0300

    Exercício

---
+++ ClasseCsharpe/Program.cs
@@ -0,0 +1,23 @@
+﻿using Classe.src;
+using System;
+
+namespace Classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bicicleta bike1 = new Bicicleta();
+            Bicicleta bike2 = new Bicicleta("Vermelho", "Médio", false);
+            Bicicleta bike3 = new Bicicleta("Azul", "Pequeno", true);
+            Bicicleta bike4 = new Bicicleta("Verde", "Grande", true);
+
+
+            bike1.Subir();
+            bike1.Pedalar();
+            bike1.Descendo();
+
+            Console.WriteLine(bike2.ToString());
+        }
+    }
+}
---
+++ ClasseCsharpe/src/Bicicleta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe.src
+{
+    internal class Bicicleta
+    {
+        string Cor { get; set; }
+        string Tamanho { get; set; } 
+        bool ModeloComRodinhas   { get; set; }
+        bool MontadoBicicleta { get; set; }
+        public Bicicleta()
+        {
+
+        }
+
+        public Bicicleta(string cor, string tamanho, bool modeloComRodinhas)
+        {
+            Cor = cor;
+            Tamanho = tamanho;
+            ModeloComRodinhas = modeloComRodinhas;
+        }
+
+
+        public void Subir()
+        {
+            Console.WriteLine("Bora fi pedala");
+            MontadoBicicleta = true;
+        }
+        public void Descendo()
+        {
+            Console.WriteLine("Cansou");
+            MontadoBicicleta = false;
+        }
+        public void Pedalar()
+        {
+            if (MontadoBicicleta == true)
+            {
+                Console.WriteLine("Acelera fi!");
+            }else
+            {
+                Console.WriteLine("Sobe na bicicleta primeiro né fi");
+            }
+        }
+        public void Freiar()
+        {
+            Console.WriteLine("TÁ Bracando! Deixa ir..");
+        }
+
+        public override string ToString()
+        {
+            return $"A cor é : {Cor}" +
+                   $"\n O tamanho é :{Tamanho}" +
+                   $"\n Possui rodinhas:{ModeloComRodinhas}";
+                
+        }
+    }
+}
